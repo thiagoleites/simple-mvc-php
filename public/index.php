ee31fd0f14669628563877e0474292ed5419905d
Thiago Leite <thiagoleites@gmail.com>
2026-05-15 23:38:57 +0000

    update public index file

--- public/index.php
+++ public/index.php
@@ -3,3 +3,11 @@
 session_start();
 
 require_once __DIR__ . "/../vendor/autoload.php";
+
+use App\Core\Router;
+
+$router = new Router();
+
+require_once __DIR__ . '/../routes/web.php';
+
+$router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
