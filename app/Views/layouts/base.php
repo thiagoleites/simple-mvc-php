63ea26fdc76051ae31b2d4af3677f3ff7c5821db
Thiago Leite <thiagoleites@gmail.com>
2026-05-15 23:36:24 +0000

    wip

---
+++ app/Views/layouts/base.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC com PHP puro</title>
+    <link rel="stylesheet" href="https://github.com/thiagoleites/frameworkcss/blob/main/style.css">
+</head>
+<body>
+    
+    <header class="main-header">
+        <div class="container">
+            <a href="#" class="logo">Logo</a>
+            <nav class="main-nav">
+                <a href="#">Início</a>
+                <a href="#">Sobre</a>
+                <a href="#">Serviços</a>
+                <a href="#">Contato</a>
+            </nav>
+        </div>
+    </header>
+
+    <?php require $pathView; ?>
+
+</body>
+</html>
