13f0e7e75290320f1accf921ec3942bbcce81a3e
Thiago Leite <thiagoleites@gmail.com>
2026-05-17 04:37:17 +0000

    wip

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+use App\Core\Auth;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        Auth::requireLevel(['admin']);
+
+        $usuarios = [
+            ['nome' => 'Thiago', 'nivel' => 'admin'],
+            ['nome' => 'João', 'nivel' => 'usuario'],
+        ];
+
+        $this->view('users/index', [
+            'usuarios' => $usuarios
+        ]);
+    }
+}
