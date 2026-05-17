8260d7e2def15d88680750cb4102162385528d01
Thiago Leite <thiagoleites@gmail.com>
2026-05-17 05:12:44 +0000

    feat: add auth controller

---
+++ app/Controllers/Authcontroller.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        $this->view('login');
+    }
+
+    public function authenticate()
+    {
+        $email = $_POST['email'] ?? '';
+        $senha = $_POST['senha'] ?? '';
+
+        if ($email === 'admin@email.com' && $senha === '123456') {
+            $_SESSION['usuario'] = [
+                'id' => 1,
+                'nome' => 'Admin',
+                'email' => $email,
+                'nivel' => 'admin'
+            ];
+
+            $this->redirect('/');
+        }
+
+        echo "Login inválido";
+    }
+
+    public function logout()
+    {
+        session_destroy();
+
+        $this->redirect('/login');
+    }
+}
