e738cc9e70c2601a5305238603e5132fb0841cb1
Thiago Leite <thiagoleites@gmail.com>
2026-05-17 05:22:02 +0000

    wip

---
+++ app/Views/login.php
@@ -0,0 +1,15 @@
+<section>
+    <h2>Login</h2>
+    <form method="post" action="">
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <input type="email" id="email" placeholder="seu.email@exemplo.com">
+        </div>
+        <div class="form-group">
+            <label for="email">Senha</label>
+            <input type="password" id="senha" placeholder="sua.senha">
+        </div>
+
+        <div class="btn btn-primary" type="button">Enviar dados</div>
+    </form>
+</section>
