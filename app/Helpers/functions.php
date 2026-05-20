043252e4e7586d3c3d3ad626955ab65a1e2cde9f
Thiago Leite <thiagoleites@gmail.com>
2026-05-20 16:59:15 +0000

    feat: implement enrironment configuration with native .env support

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -14,3 +14,36 @@ function __(mixed $key)
     return $langArray[$key] ?? $key;
     
 }
+
+function loadEnv(mixed $path)
+{
+    if (!file_exists($path)) {
+        throw new Exception(".env não encontrado");
+    }
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($lines as $line) {
+        // Ignora comentários
+        if (str_starts_with(trim($line), '#')) {
+            continue;
+        }
+
+        // Ignora linhas inválidas
+        if (!str_contains($line, '=')) {
+            continue;
+        }
+
+        [$key, $value] = explode('=', $line, 2);
+
+        $key = trim($key);
+        $value = trim($value);
+
+        // remove aspas
+        $value = trim($value, '"');
+
+        $_ENV[$key] = $value;
+
+        putenv("$key=$value");
+    }
+}
--- public/index.php
+++ public/index.php
@@ -7,7 +7,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . "/../vendor/autoload.php";
-require_once __DIR__ . '/../app/Helpers/functions.php';
+
+
+loadEnv('../.env');
+
 
 use App\Core\Router;
 
